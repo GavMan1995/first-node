2c3758a70022e68965182eb13a0d4d1944c92107
GavMan1995 <gavyncaldwell@gmail.com>
2017-01-05 16:42:39 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+let app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('serer.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res) => {
+  res.render('maint.hbs')
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', {
+    title: 'Home Page',
+    message: 'Hey there how are you doing?',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'About page',
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send(
+    {
+      message: 'ERROR ERROR BAD REQUEST'
+    }
+  )
+})
+
+app.listen(8080, () => {
+  console.log('Server is up on port 8080')
+})
